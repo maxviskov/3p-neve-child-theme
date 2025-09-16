2f9a210938e39fb3af73f0ca12d250f2d95602bc
maxviskov <maxviskov@gmail.com>
2025-09-16 10:55:50 -0700

    Update page-wedding.php

--- page-templates/page-wedding.php
+++ page-templates/page-wedding.php
@@ -1,1 +1,13 @@
-﻿
+﻿<?php get_header(); ?>
+
+<div style="padding: 40px 0;">
+    <div class="container">
+        <h1>Free Personalized Wedding Planner</h1>
+        <p>Get your customized wedding plan.</p>
+        
+        <div data-tf-live="01K1NSRF64B55KK37BK85TACHR"></div>
+        <script src="//embed.typeform.com/next/embed.js"></script>
+    </div>
+</div>
+
+<?php get_footer(); ?>
